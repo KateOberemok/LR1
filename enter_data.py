654da4da0b932a0f93f5854ebd9b9a1f62483e0c
KateOberemok <Oberemok.K@yandex.ua>
2018-05-09 08:50:49 +0300

    Create enter_data.py

---
+++ enter_data.py
@@ -0,0 +1,27 @@
+def identity_matrix(n): #единичная матрица
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(n):
+            if i == j:
+                matrix[i].append(1)
+            else:
+                matrix[i].append(0)
+    return matrix
+
+def zero_matrix(n): #единичная матрица
+    matrix = [[0]*n]*n
+    return matrix
+
+def enter_matrix(n): #ввод матрицы nxn
+    matrix = []
+    for i in range(n):
+        list = (input("введите " + str(i + 1) + "-ю строку через пробел: ")).split()
+        if len(list) != n+1:
+            print ("error")
+            break
+        else:
+            for element in range(len(list)):
+                list[element] = int(list[element])
+            matrix.append(list)
+    return matrix
