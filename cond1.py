e5982501e30d8b01ef7e625fe48ba8b5de2e2810
KateOberemok <Oberemok.K@yandex.ua>
2018-05-10 00:11:44 +0300

    Create cond1.py

---
+++ cond1.py
@@ -0,0 +1,31 @@
+import math
+
+def cond1(matrix, inv_matrix, n):
+    matrix_vector = [0] * n
+    inv_matrix_vector = [0] * n
+    matrix_vector = sum(matrix, matrix_vector)
+
+    inv_matrix_vector = sum(inv_matrix, inv_matrix_vector)
+
+    number_A = maximum(matrix_vector)
+
+    number_inv_A = maximum(inv_matrix_vector)
+
+    cond = number_A * number_inv_A
+    return cond
+
+def sum(matrix, vector):
+    n = len(vector)
+    for i in range(n):
+        for j in range(n):
+            vector[i] += math.fabs(matrix[j][i])
+    return vector
+
+def maximum(vector):
+    n = len(vector)
+    max = vector[0]
+    for i in range(1, n):
+        if vector[i] > max:
+            max = vector[i]
+    return max
+
